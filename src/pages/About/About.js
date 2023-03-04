af64d2525f740356f888889f0357d29a74689555
Gaurav <gauravburande2425@gmail.com>
2023-03-04 12:30:24 +0530

    About Container Added

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,30 @@
 import React from "react";
-
+import styles from "../About/About.module.css";
+import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
 function About() {
   return (
     <div>
-      <div>About</div>
+      <Navbar></Navbar>
+      <div className={styles.container1}>
+        <div className={styles.container1_left}>
+          <img src={require("../../assets/about_img.png")}></img>
+        </div>
+
+        <div className={styles.container1_right}>
+          <h1>Find your tribe, and love them hard</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            iaculis iaculis libero et pretium. Etiam viverra a tellus sed
+            iaculis. Vivamus nisi orci, pulvinar eget bibendum vitae.Everyone
+            has their own way of learning and expressing creativity. Apple
+            technology and resources empower every kind of educator — and every
+            kind of student — to learn, create and define their own success.
+            Let’s move the world forward.
+          </p>
+        </div>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
