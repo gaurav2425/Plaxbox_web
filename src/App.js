80359386747d0e3027673865444c38a4f3e7f71e
iamgaurav18 <gaurav.burande@blockstack.tech>
2025-01-21 18:04:00 +0530

    Added google analytics tag

--- src/App.js
+++ src/App.js
@@ -9,7 +9,28 @@ import Social from "./pages/Social/Social";
 import About from "./pages/About/About";
 import Navbar from "./Components/Navbar";
 function App() {
-  console.log(window.pageYOffset);
+  useEffect(() => {
+    // Load gtag.js
+    const script1 = document.createElement("script");
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-WTY116E7NT";
+    script1.async = true;
+    document.head.appendChild(script1);
+
+    // Add gtag configuration
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-WTY116E7NT');
+    `;
+    document.head.appendChild(script2);
+
+    return () => {
+      document.head.removeChild(script1);
+      document.head.removeChild(script2);
+    };
+  }, []);
   return (
     <Router>
       <Routes>
